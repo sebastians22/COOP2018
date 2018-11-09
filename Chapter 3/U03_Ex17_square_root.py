3cb6575aafea6957f880620f07e45b38134c2166
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2018-11-08 22:49:56 -0600

    I did the header, im squeezing out as many points as I can get

---
+++ Chapter 3/U03_Ex17_square_root.py
@@ -0,0 +1,24 @@
+# U03_Ex17_square_root.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 08 Nov 2018
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 17
+#     Source: Python Programming
+#    Chapter: 3
+#
+# Program Description
+#
+#
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
+#
+#
