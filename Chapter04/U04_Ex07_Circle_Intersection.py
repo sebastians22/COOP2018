9988f290ec4d9d871053c6f3c0058053d49d6284
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2018-11-09 14:58:23 -0600

    Fixed for better grade

---
+++ Chapter04/U04_Ex07_Circle_Intersection.py
@@ -0,0 +1,24 @@
+# U04_Ex07_Circle_Intersection.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 09 Nov 2018
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 7
+#     Source: Python Programming
+#    Chapter: 4
+#
+# Program Description
+# This program will draw the radius and the y intercept through a line of a circle
+#
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
+#
+#
