fa729d0a9f9605a9124137c358834da0bdd9df56
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2018-11-09 14:02:43 -0600

    Fixed for better grade

---
+++ Chapter04/U04_Ex08_Line_Segmants.py
@@ -0,0 +1,52 @@
+# U04_Ex08_Line_Segmants.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 09 Nov 2018
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 9
+#     Source: Python Programming
+#    Chapter: 04
+#
+# Program Description
+#  This program gives the area and perimeter of a rectangle
+#
+#
+#
+# Algorithm (pseudocode)
+# Get p1 and p2 from the user to make a rectangle
+#
+#
+#
+#
+
+from graphics import *
+
+def main():
+    win = GraphWin()
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+
+    rect = Rectangle(p1, p2)
+    rect.draw(win)
+
+    x1 = p1.getX()
+    y1 = p1.getY()
+    x2 = p2.getX()
+    y2 = p2.getY()
+
+    length = x1 + x2
+    width = y1 + y2
+
+    area = length * width
+    print("The area of your rectangle is", area, "pixels")
+
+    perimeter = 2 * (length + width)
+    print("The perimeter of your rectangle is", perimeter, "pixels")
+
+    input("Press ENTER to continue")
+
+main()
