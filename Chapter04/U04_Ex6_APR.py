ea7b85bbc037eed0869921efcd7839fe7f679395
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2018-11-09 14:56:11 -0600

    Fixed for better grade

---
+++ Chapter04/U04_Ex6_APR.py
@@ -0,0 +1,24 @@
+# U04_Ex6_APR.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 09 Nov 2018
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 6
+#     Source: Python Programming
+#    Chapter: 4
+#
+# Program Description
+# This program will modify the graphical future value program to add graphics
+#
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
+#
+#
