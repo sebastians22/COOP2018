9265a7e9a927093970cdfad2e43e085010632abf
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-01-23 11:08:48 -0600

    Here is exercise 55

--- Chapter04/U04_Ex6_APR.py
+++ Chapter04/U04_Ex6_APR.py
@@ -17,8 +17,28 @@
 #
 #
 # Algorithm (pseudocode)
+# print the intro to the program
+# have the user enter the principle
+# have the user enter the apr
+# have the user enter how many years they would be investing into the bank
+# print a table
+# have a for loop years = to years
 #
-#
-#
-#
-#
+
+def main():
+
+    print("This program will calculate the future value over 10 years")
+
+    principle = eval(input("Enter the annul interest rate"))
+    apr = eval(input("Enter the annul interest rate"))
+    years = eval(input("Enter the number of years you will be investing"))
+
+    print("Year/t Value")
+    print("----/t------")
+
+    for years in range(years):
+        principle = principle * (1 + apr)
+
+
+
+main()
