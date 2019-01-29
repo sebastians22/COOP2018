6465c145c40d9ace704e20f5e3c845fe1bd1bc81
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-01-29 10:15:38 -0600

    This is junk

---
+++ Chapter06/U06_Ex01_Mcdonald's_Farm.py
@@ -0,0 +1,48 @@
+# U06_Ex01_Mcdonald's_Farm.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 16 Jan 2019
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise:
+#     Source: Python Programming
+#    Chapter:
+#
+# Program Description
+#
+#
+#
+#
+# Algorithm (pseudocode)
+#
+#
+#
+#
+#
+
+
+def main():
+
+    mcdonald("pig")
+
+
+def mcdonald(animal):
+
+    print("Old mcdonald had a farm Ee-igh, Ee-igh, Oh!")
+    print("And on that farm he had a", animal + ".")
+    print("With a", animalsound(), animalsound(), "here and a", animalsound(), animalsound(), "there")
+    print("Here a", animalsound(), "there a", animalsound(), "everywhere a", animalsound(), animalsound() + ".")
+    print("Old mcdonald had a farm Ee-igh, Ee-igh, Oh!")
+    print()
+
+
+def animalsound():
+
+
+    return "oink"
+
+
+main()
