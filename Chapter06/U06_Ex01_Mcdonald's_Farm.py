66086a1b633ab17df36b15d79d72c1e8fd29b496
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-02-08 11:53:37 -0600

    Fixed it

--- Chapter06/U06_Ex01_Mcdonald's_Farm.py
+++ Chapter06/U06_Ex01_Mcdonald's_Farm.py
@@ -7,42 +7,48 @@
 #   IDE: PyCharm
 #
 # Assignment Info
-#   Exercise:
+#   Exercise: 1
 #     Source: Python Programming
-#    Chapter:
+#    Chapter: 6
 #
 # Program Description
-#
+# This program will print Old Mcdonalds Farm using functions
 #
 #
 #
 # Algorithm (pseudocode)
+# def main():
+# have mcdonald(animal, sound) for the function, type this 5 times
+# put a different animal and different sound for each mcdonald()
 #
-#
-#
-#
-#
+# def mcdonald(animal, sound):
+# print the full version of Old Mcdonalds Farm, and every time there is a part with an animal, put animal for animal
+#   and sound for animal sound
+# print a line after the last line to make it easier to read for the user
+
 
 
 def main():
 
-    mcdonald("pig")
+    mcdonald("pig", "oink")
+
+    mcdonald("cow", "moo")
+
+    mcdonald("Llama", "Yeet")
+
+    mcdonald("Lion", "Roar")
+
+    mcdonald("Dog", "Bark")
 
 
-def mcdonald(animal):
+def mcdonald(animal, sound):
 
     print("Old mcdonald had a farm Ee-igh, Ee-igh, Oh!")
     print("And on that farm he had a", animal + ".")
-    print("With a", animalsound(), animalsound(), "here and a", animalsound(), animalsound(), "there")
-    print("Here a", animalsound(), "there a", animalsound(), "everywhere a", animalsound(), animalsound() + ".")
+    print("With a", sound, sound, "here and a", sound, sound, "there")
+    print("Here a", sound, "there a", sound, "everywhere a", sound, sound + ".")
     print("Old mcdonald had a farm Ee-igh, Ee-igh, Oh!")
     print()
-
-
-def animalsound():
-
-
-    return "oink"
 
 
 main()
