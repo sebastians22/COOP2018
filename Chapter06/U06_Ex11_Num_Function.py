cdc489e998c369947c3f5f5c47a46d79d6810baa
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-02-14 10:31:41 -0600

    Only one problem with

---
+++ Chapter06/U06_Ex11_Num_Function.py
@@ -0,0 +1,38 @@
+# U06_Ex11_Num_Function.py
+#
+#  Author: Sebastian Schaefer
+#  Course: Coding for OOP
+# Section: A2
+#   Date: 06 Feb 2019
+#   IDE: PyCharm
+#
+# Assignment Info
+#   Exercise: 11
+#     Source: Python Programming
+#    Chapter: 6
+#
+# Program Description
+# This program will square a num using fuctions
+#
+#
+#
+# Algorithm (pseudocode)
+# Get the num from the user using eval input
+# print using function
+#
+# def number(num)
+# return the number squared
+
+
+def main():
+
+    num = eval(input("What is your number?: "))
+    print(number(num))
+
+
+def number(num):
+
+    return num ** 2
+
+
+main()
