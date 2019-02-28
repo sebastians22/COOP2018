4a9d12efbfbf6393cd3f26cc716100de078ea24f
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-02-28 14:36:24 -0600

    Fixed it

--- Chapter05/U05_Ex06_Numerology.py
+++ Chapter05/U05_Ex06_Numerology.py
@@ -24,10 +24,8 @@
 # split the lower_name, use new_name as a variable
 # write out the alphabet and make a capital X before the start to account for -1, name that letters (Xabcd....)
 # Make word_value worth 0
-# Make char worth 0
 # Join the new_name, have it named new_new_name
 # Make a loop for char in new_name
-# Make a letter_value worth 0
 # Find the new_new_name
 # Make word_value = to word_value + letter_value
 # Print the value of your name is (new_new_name)
@@ -35,16 +33,17 @@
 def main():
     name = input("Please enter a name ")
     lower_name = name.lower().replace(" ", "")
+
     letters = "Xabcdefghijklmnopqrstuvwxyz"
-    letter_value = 0
     word_value = 0
-    char = 0
+
     for char in lower_name:
+
         name.find(lower_name)
         letter_value = letters.index(char)
         word_value = word_value + letter_value
 
-    print("The value of your name is", word_value)
+    print("The value of your name(", name, ") is", word_value)
 
 
 main()
