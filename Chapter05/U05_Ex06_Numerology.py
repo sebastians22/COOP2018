7691f19ce4fd132d3c7d53db76323f5e463314e7
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-02-28 14:33:13 -0600

    Fixed it!

--- Chapter05/U05_Ex06_Numerology.py
+++ Chapter05/U05_Ex06_Numerology.py
@@ -20,6 +20,7 @@
 # Algorithm (pseudocode)
 # Have the user enter a name, use name as a variable
 # Make all the letters of what the user entered to lowercase, use lower_name as a variable
+# get rid of the spaces
 # split the lower_name, use new_name as a variable
 # write out the alphabet and make a capital X before the start to account for -1, name that letters (Xabcd....)
 # Make word_value worth 0
@@ -32,27 +33,18 @@
 # Print the value of your name is (new_new_name)
 
 def main():
-    print("This program will show the numeric value of your name,numerologists claim that they "
-          "can determine a persons character traits based on the numeric value of a persons name")
-    # Have the user enter a name, use name as a variable
     name = input("Please enter a name ")
-    # Make all the letters of what the user entered to lowercase, use lower_name as a variable
-    lower_name = name.lower()
-    # split the lower_name, use new_name as a variable
-    new_name = lower_name.split()
-    # write out the alphabet and make a capital X before the start to account for -1, name that letters (Xabcd....)
+    lower_name = name.lower().replace(" ", "")
     letters = "Xabcdefghijklmnopqrstuvwxyz"
+    letter_value = 0
     word_value = 0
     char = 0
-    letter_value = 0
-    # Join the new_name, have it named new_new_name
-    new_new_name = (''.join(new_name))
-
-    for char in new_name:
-        name.find(new_new_name)
+    for char in lower_name:
+        name.find(lower_name)
+        letter_value = letters.index(char)
         word_value = word_value + letter_value
-    # Print the value of your name is (new_new_name)
-    print(name, ", The numeric value of your name is", new_new_name, ".")
+
+    print("The value of your name is", word_value)
 
 
 main()
