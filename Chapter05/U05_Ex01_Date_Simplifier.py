c8ce2a0c24b8065e2ac4a07148b8dadca7d5d173
Sebastian Schaefer <sebastian.s22@student.parishepiscopal.org>
2019-02-28 23:27:16 -0600

    I think I got it

--- Chapter05/U05_Ex01_Date_Simplifier.py
+++ Chapter05/U05_Ex01_Date_Simplifier.py
@@ -18,13 +18,12 @@
 #
 # Algorithm (pseudocode)
 # Print what the program is about, use a shorter version of the program description
-# Use a string to get "monthStr", "dayStr", and "yearStr" = dateStr
-# print a blank line to make the code easier to read
-# print month/day/year
-# print a blank line to make the code easier to read
-# Make a list with every month in order
-# Subtract the value by 1 to make the months and what the user input to line up
-# Print the date using a string
+# ask the user to input a date in mm/dd/yyyy format
+# split it with ("/")
+# make a string with all the months
+# makes monthStr = moths[int(monthStr)-1]
+# print a blank like so it's easier to read
+# print the outro
 
 def main():
     print("This program will take your birthday and put it into simpler terms, then it will convert it into a nice form")
@@ -33,18 +32,11 @@ def main():
 
     monthStr, dayStr, yearStr = dateStr.split("/")
 
-    print("")
-
-    print("You where born on", monthStr, "/", dayStr, "/", yearStr)
-
-    print("")
-
     months = ("January", "February", "March", "April", "May", "June", "July",
               "August", "September", "October", "November", "December")
 
     monthStr = months[int(monthStr)-1]
-
-    print("The converted date is", monthStr, dayStr, yearStr)
-
+    print("")
+    print("The date you entered was", dateStr, "The converted date is", monthStr, dayStr, yearStr)
 
 main()
